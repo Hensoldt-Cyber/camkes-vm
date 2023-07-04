5c57343d2ffa90d8654af684245a8ec4035e9de3
Felix Schladt <felix.schladt@hensoldt.net>
2023-07-04 14:19:52 +0200

    dump generated device tree

--- components/VM_Arm/plat_include/zynqmp/plat/vmlinux.h
+++ components/VM_Arm/plat_include/zynqmp/plat/vmlinux.h
@@ -14,18 +14,10 @@ static const int free_plat_interrupts[] =  { -1 };
 
 static const char *plat_keep_devices[] = {
     "/timer",
-    "/psci",
-    "/pmu",
-    "/amba/ethernet@ff0b0000",
-    "/amba/ethernet@ff0c0000",
-    "/amba/ethernet@ff0d0000",
-    "/amba/serial@ff010000"
 };
 static const char *plat_keep_device_and_disable[] = {};
 static const char *plat_keep_device_and_subtree[] = {
     GIC_NODE_PATH,
-    "/amba/ethernet@ff0e0000",
-    "/amba/pcie@fd0e0000",
 };
 static const char *plat_keep_device_and_subtree_and_disable[] = {};
 static const char *plat_linux_bootcmdline = "";
